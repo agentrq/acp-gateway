ccd35577bee5df723a619a92d00261d2857e6e97
Mustafa Turan <mustafaturan.net@gmail.com>
2026-06-05 01:49:42 -0700

    0.1.22

--- src/mcpClient.ts
+++ src/mcpClient.ts
@@ -84,7 +84,7 @@ export class MCPBridge extends EventEmitter {
     this.client = new Client(
       {
         name: "acp-gateway",
-        version: "0.1.21",
+        version: "0.1.22",
       },
       {
         capabilities: {},
