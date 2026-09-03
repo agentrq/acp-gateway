0ff06a77d12e2586c688179fb26772b1c8b9556c
Mustafa Turan <mustafaturan.net@gmail.com>
2026-09-03 00:08:49 -0700

    chore: bump version to 0.2.7
    TaskID: 0i8DnZABNnV

--- README.md
+++ README.md
@@ -42,7 +42,7 @@ npm install -g @agentrq/acp-gateway
 
 ## Current Version
 
-`0.2.6`
+`0.2.7`
 
 ## Usage
 
--- src/mcpClient.ts
+++ src/mcpClient.ts
@@ -96,7 +96,7 @@ export class MCPBridge extends EventEmitter {
     this.client = new Client(
       {
         name: "acp-gateway",
-        version: "0.2.6",
+        version: "0.2.7",
       },
       {
         capabilities: {},
