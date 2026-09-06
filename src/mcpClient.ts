f8a44c1a220e216be476b574b39d979d7d3edb11
Mustafa Turan <mustafaturan.net@gmail.com>
2026-09-05 17:25:02 -0700

    chore: bump version to 0.2.10 (TaskID: 0iHCmceBd45)

--- src/mcpClient.ts
+++ src/mcpClient.ts
@@ -96,7 +96,7 @@ export class MCPBridge extends EventEmitter {
     this.client = new Client(
       {
         name: "acp-gateway",
-        version: "0.2.9",
+        version: "0.2.10",
       },
       {
         capabilities: {},
